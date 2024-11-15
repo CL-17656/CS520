9b722b9e979acc5213aa45ba69ced30a796661c9
bertiesh <bertiesh98@gmail.com>
2024-11-15 14:23:38 -0500

    Initial commit

---
+++ src/main/java/org/example/cs520/dto/UserBackDTO.java
@@ -0,0 +1,44 @@
+package org.example.cs520.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Xinyuan Xu
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserBackDTO {
+    /**
+     * id
+     */
+    private Integer id;
+
+    private Integer userInfoId;
+
+    private String avatar;
+
+    private String nickname;
+
+    private List<UserRoleDTO> roleList;
+
+    private Integer loginType;
+
+    private String ipAddress;
+
+    private String ipSource;
+
+    private Date createTime;
+
+    private Date lastLoginTime;
+
+    private Integer status;
+}
+
