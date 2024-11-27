3fc222d928826f21f50f4b4b274ea65110c825a7
bertiesh <bertiesh98@gmail.com>
2024-11-27 16:32:02 -0500

    add post searching

---
+++ src/main/java/org/example/cs520/dto/PostBackDTO.java
@@ -0,0 +1,42 @@
+package org.example.cs520.dto;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Xinyuan Xu
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostBackDTO {
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * respondent id
+     */
+    private Integer userId;
+
+    private Integer projectId;
+
+    private String answer;
+
+    private Integer isDelete;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+}
+
+
